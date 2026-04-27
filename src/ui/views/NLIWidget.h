2a76103f5836b04bd863788ff623d8ddb99f6778
SIEL <114921056+SimpungaElias@users.noreply.github.com>
2026-04-27 10:45:21 +0100

    Refactor NLIWidget class member variables

--- src/ui/views/NLIWidget.h
+++ src/ui/views/NLIWidget.h
@@ -1,6 +1,5 @@
 #ifndef NLIWIDGET_H
 #define NLIWIDGET_H
-
 #include <QWidget>
 #include <QLineEdit>
 #include <QComboBox>
@@ -12,51 +11,33 @@
 #include <QLabel>
 #include <QPushButton>
 #include <QDateEdit>
+#include <QCompleter>
 
 class NLIWidget : public QWidget {
     Q_OBJECT
-
 public:
     explicit NLIWidget(QWidget *parent = nullptr);
     void loadData();
-
 private slots:
     void addEntry();
     void exportPDF();
     void deleteEntry();
-
 private:
     void setupUI();
     void setupReportHeader(QVBoxLayout *layout);
     void setupInputForm(QVBoxLayout *layout);
     void setupTable(QVBoxLayout *layout);
-    
-
-    // Report Header Inputs
-    QLineEdit *txtCountry;
-    QLineEdit *txtFacility;
-    QLineEdit *txtMBA;
-    QDateEdit *dateReport;
-    QLineEdit *txtReportNo;
-
-    // Transaction Inputs
-    QLineEdit *txtBatch;
-    QSpinBox *spinItems;
-    QComboBox *comboCode;
-    
-    // Uranium Inputs
-    QLineEdit *txtUElemCode;
-    QLineEdit *txtUIsoCode;
-    QDoubleSpinBox *spinUWeight;
-    QDoubleSpinBox *spinUIsoWeight;
-
-    // Plutonium Inputs
-    QLineEdit *txtPElemCode;
-    QDoubleSpinBox *spinPWeight;
-
-    // Display
+    QComboBox    *comboCountry;   // searchable IAEA list
+    QLineEdit    *txtFacility;
+    QComboBox    *comboMBA;
+    QDateEdit    *dateReport;
+    QSpinBox     *spinReportNo;   // number only (was QLineEdit)
+    QLineEdit    *txtBatch;
+    QSpinBox     *spinItems;
+    QComboBox    *comboCode;
+    QLineEdit    *txtUElemCode, *txtUIsoCode, *txtPElemCode;
+    QDoubleSpinBox *spinUWeight, *spinUIsoWeight, *spinPWeight;
     QTableWidget *table;
     int lineCounter;
 };
-
-#endif // NLIWIDGET_H
+#endif
